ddc54dceeeb0ca69a7b694d42bcdba2c697b0c46
Tanapong Tirapattharachai <tanapong.t@arise.tech>
2024-03-20 09:23:27 +0700

    sales-by-match

---
+++ hacker-rank/sales-by-match.go
@@ -0,0 +1,34 @@
+// https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true
+
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func sockMerchant2(n int32, ar []int32) int32 {
+	var pairs int
+	sort.Slice(ar, func(i, j int) bool {
+		return ar[i] < ar[j]
+	})
+
+	i := 0 // Initialize i to 0
+	for i < len(ar)-1 {
+		if ar[i] == ar[i+1] {
+			pairs++
+			i += 2 // Move to the next pair
+		} else {
+			i++ // Move to the next element
+		}
+	}
+
+	return int32(pairs)
+}
+
+func main() {
+	n := int32(7) // Converting to int32
+	ar := []int32{1, 2, 1, 2, 1, 3, 2}
+	result := sockMerchant2(n, ar)
+	fmt.Println(result)
+}
